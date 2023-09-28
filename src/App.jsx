4ff593ea8d99becc84924d6269007299ea6fd959
Aurel Npounengnong <aurelbrondon@gmail.com>
2023-09-28 15:13:46 -0400

    working portfolio

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter } from "react-router-dom";
+import {About,Contact,Experience,Feedbacks,
+  Hero,Navbar,Tech,Works,StarsCanvas} from './components';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div classname="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repea bg-center">
+        <Navbar/> 
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className="relative z-0">
+        <contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App 
+
+//remember do do: npm run build first. had a weird vite error there for a sec
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    console.log()
+  )
+}
+
+export default Loader
